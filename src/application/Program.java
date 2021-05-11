8fc7d3ed28a4b4ca96a859e0c1724912a62e0086
rogerpert <rogertyago@gmail.com>
2021-05-11 18:34:00 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -24,6 +24,12 @@ public class Program {
 			System.out.println(obj);
 		}
 		
+		System.out.println("\n=== Test 3: seller findByAll ===");
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+		
 	}
 
 }
